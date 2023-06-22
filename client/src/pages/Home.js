218ad02cbf5347024b519ed7d022f1b50583f11b
Nya Tyus <nyastyus@gmail.com>
2023-06-22 01:28:27 -0400

    add image banner and overlay to home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,5 +1,17 @@
 import React from "react";
+import "../styles/Home.css";
 
 export default function Home() {
-  return <div>home</div>;
+  return (
+    <div>
+      <div className="banner-container">
+        <div className="d-flex flex-row">
+          <img src="/images/gym1.jpg" alt="" className="home-img" />
+          <img src="/images/gym2.jpg" alt="" className="home-img" />
+          <img src="/images/gym3.jpg" alt="" className="home-img" />
+        </div>
+        <div className="overlay">Embrace the Journey, Unleash Your Strength</div>
+      </div>
+    </div>
+  );
 }
